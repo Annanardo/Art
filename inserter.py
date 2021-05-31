2ea09ba1d380752f0deac6efbb37674395e6a76e
Annanardo <annanardo00@gmail.com>
2021-05-31 15:56:36 +0200

    updated inserter

--- inserter.py
+++ inserter.py
@@ -1,10 +1,10 @@
 """
 The function of the inserter module
-is to append new artists with relative
+allows to append new artists with relative
 most famous painting into our database.
 The function first check if the input 
 we want to insert is already present in
-the database, if not it asks weather 
+the database, if not it asks whether 
 you are inserting the name of an artist 
 or of a painting.
 After that it asks you to add all the 
