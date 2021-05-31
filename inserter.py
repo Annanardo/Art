d84a12db2c940721b6039b4bf0efee5f86e5344f
Annanardo <annanardo00@gmail.com>
2021-05-31 15:46:01 +0200

    description inserter function

---
+++ inserter.py
@@ -0,0 +1,16 @@
+"""
+The function of the inserter module
+is to append new artists with relative
+most famous painting into our database.
+The function first check if the input 
+we want to insert is already present in
+the database, if not it asks weather 
+you are inserting the name of an artist 
+or of a painting.
+After that it asks you to add all the 
+other additional information, to generate 
+a list of 10 items.
+Finally the list is inserter in a new 
+row becoming part
+of our database.
+"""
