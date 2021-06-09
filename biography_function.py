f804345a5d0eecb352f84284dc7975b6c36ee65b
Auroramenegatto <aurora.menegatto.0098@gmail.com>
2021-06-09 14:12:19 +0200

    Biography

---
+++ biography_function.py
@@ -0,0 +1,15 @@
+"""
+The function of the biography module
+is to explain, in a more understandable way,
+the life of artists with relative
+most famous painting, the number of artworks that
+has been made, movement, day of birth and death
+that can be found into our database.
+The function first check if the input
+we inserted is present in
+the database, if not the system will warn you
+and invite you to check if you wrote it correctly.
+
+#You can also check the full biography with
+the Wikipedia's link.
+"""
