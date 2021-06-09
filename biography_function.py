074f128b4d206bd6d9bfbcf51b778c7cf2f47798
Auroramenegatto <aurora.menegatto.0098@gmail.com>
2021-06-09 18:28:06 +0200

    PEP8

--- biography_function.py
+++ biography_function.py
@@ -17,7 +17,6 @@ the Wikipedia's link.
 import csv
 import pandas as pd
 
-   
 def return_bio(Artist):
     """
     This function comes into play once the user inputs a value
@@ -31,14 +30,13 @@ def return_bio(Artist):
 
     if Artist in Artists:
         print(db["Name"].loc[db["Name"].str.lower() == Artist.lower()].values[0],"is a/an", db["Nationality"].loc[db["Name"].str.lower() ==
-            Artist.lower()].values[0], "artist that has lived in these years:",db["Life"].loc[db["Name"].str.lower() ==
-            Artist.lower()].values[0], ". The artist painted in", db["Year"].loc[db["Name"].str.lower() ==
-              Artist.lower()].values[0],"his/her most famous painting, which is:",db["Artwork"].loc[db["Name"].str.lower() ==
-              Artist.lower()].values[0], ", is now displayed at the", db["Museum"].loc[db["Name"].str.lower() ==
-              Artist.lower()].values[0],". The painter belongs to: ", db["Genre"].loc[db["Name"].str.lower() ==
-              Artist.lower()].values[0], "movement(s). Together with his/her most famous painting, he/she realized", db["Paintings"].loc[db["Name"].str.lower() ==
-              Artist.lower()].values[0], "artworks in total. Here you can find the web link to see the complete biography: ", db["wikipedia"].loc[db["Name"].str.lower() ==
-              Artist.lower()].values[0])
+            Artist.lower()].values[0],  "artist that has lived in these years:", db["Life"].loc[db["Name"].str.lower() ==
+            Artist.lower()].values[0], ". The artist painted in",  db["Year"].loc[db["Name"].str.lower() ==
+            Artist.lower()].values[0],"his/her most famous painting, which is:", db["Artwork"].loc[db["Name"].str.lower() ==
+            Artist.lower()].values[0], ", is now displayed at the",  db["Museum"].loc[db["Name"].str.lower() ==
+            Artist.lower()].values[0],". The painter belongs to: ", db["Genre"].loc[db["Name"].str.lower() ==
+            Artist.lower()].values[0], "movement(s). Together with his/her most famous painting, he/she realized", db["Paintings"].loc[db["Name"].str.lower() ==
+            Artist.lower()].values[0], "artworks in total. Here you can find the web link to see the complete biography: ", db["wikipedia"].loc[db["Name"].str.lower() ==
+            Artist.lower()].values[0])
     else:
-        print(Artist + " seems not to be present in our database. Are you sure that you wrote the name of the artist correctly? Use -d to check if it is already in our database, maybe there is a spelling error in the input. Check it and then try again, if it is not present, use -a to insert the new artist, thank you for your patience!")
-            
+        print(Artist + " seems not to be present                               in our database. Are you sure that                                you wrote the name of the artist correctly?                        Use -d to check if it is already in our database,                maybe there is a spelling error in the input.                     Check it and then try again, if it is not present,                 use -a to insert the new artist, thank you for your patience!")
